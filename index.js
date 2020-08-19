242a32a667f93a6527162967359177fedf811a7d
天天 <wangxiang@yuanbaobaoxian.com>
2020-08-19 15:34:16 +0800

    webpack4+处理方案

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
+const fs = require('fs');
+
 module.exports = class Rename {
   constructor(options) {
+    console.log('========rename init=======')
     this.options = { ...options };
   }
 
   apply(compiler) {
-    compiler.hooks.emit.tap('Rename.emit', compilation => {
+    console.log('========rename apply=======')
+
+    compiler.hooks.afterEmit.tap('Rename.emit', compilation => {
       const { assets } = compilation;
       const { originNameReg, targetName } = this.options;
-      const originFiles = Object.keys(assets).filter(fileName =>
-        new RegExp(originNameReg).test(fileName)
-      );
-
+      const reg = new RegExp(originNameReg);
+      const originFiles = Object.keys(assets).filter(fileName => {
+        // console.log(fileName);
+        return reg.test(fileName);
+      });
+      console.log('===========', originFiles);
       originFiles.forEach(fileName => {
-        assets[fileName.replace(originNameReg, targetName)] = assets[fileName];
-        delete assets[fileName];
+        const _oldName = assets[fileName].existsAt;
+        const _newName = _oldName.replace(originNameReg, targetName);
+        console.log(_oldName, _newName);
+        fs.rename(_oldName, _newName, (res) => {
+          console.log(res);
+        });
       });
     });
   }
